f68f058466095bcc64b3231274bcdfdb4cfa9926
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2026-02-14 23:20:46 +0530

    add gradient boosting predictions,

--- implements/wl_aksvd.py
+++ implements/wl_aksvd.py
@@ -2,7 +2,7 @@ from dict_learners.aksvd import AKSVD
 from graph_encoders.wl import WL
 from utils.load_data import load_data
 from utils.evaluator import Evaluator
-graphs, y = load_data(name="reddit", size=2)
+graphs, y = load_data(name="nci", size=2)
 
 wl = WL(graphs = graphs)
 y_embedding = wl.fit()
@@ -13,4 +13,5 @@ evaluator = Evaluator(X, y, test_size=0.2)
 
 results_logistic_reg = evaluator.predict_logistic_regression()
 print(results_logistic_reg)
-
+results_gradient_boosting = evaluator.predict_gradient_boosting()
+print(results_gradient_boosting)
--- utils/evaluator.py
+++ utils/evaluator.py
@@ -1,7 +1,10 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
-
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import GradientBoostingClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 class Evaluator:
     def __init__(self, X, y, test_size=0.2, random_state=42):
@@ -22,6 +25,40 @@ class Evaluator:
         print("Predicting with Logistic Regression")
         model = LogisticRegression(random_state=0).fit(self.X_train, self.y_train)
         y_hat = model.predict_proba(self.X_test)[:, 1]
+        y_pred = model.predict(self.X_test)
+        cm = confusion_matrix(self.y_test, y_pred)
         auc = roc_auc_score(self.y_test, y_hat)
-        print('Without overfit protection')
+
+        # Visualize confusion matrix
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                    xticklabels=['Class 0', 'Class 1'],
+                    yticklabels=['Class 0', 'Class 1'])
+        plt.title('Confusion Matrix - With Overfit Protection - Logistic Regression\nAUC: {:.4f}'.format(auc))
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+        plt.tight_layout()
+        plt.savefig('./cm/confusion_matrix_logistic_regression.png', dpi=300, bbox_inches='tight')
+        plt.show()
+        return 'AUC: {:.4f}'.format(auc)
+
+    def predict_gradient_boosting(self):
+        print("Predicting with Gradient Boosting")
+        model = GradientBoostingClassifier(random_state=0).fit(self.X_train, self.y_train)
+        y_hat = model.predict_proba(self.X_test)[:, 1]
+        y_pred = model.predict(self.X_test)
+        cm = confusion_matrix(self.y_test, y_pred)
+        auc = roc_auc_score(self.y_test, y_hat)
+
+        # Visualize confusion matrix
+        plt.figure(figsize=(8, 6))
+        sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
+                    xticklabels=['Class 0', 'Class 1'],
+                    yticklabels=['Class 0', 'Class 1'])
+        plt.title('Confusion Matrix - With Overfit Protection - Gradient Boosting\nAUC: {:.4f}'.format(auc))
+        plt.ylabel('True Label')
+        plt.xlabel('Predicted Label')
+        plt.tight_layout()
+        plt.savefig('./cm/confusion_matrix_gradient_boosting.png', dpi=300, bbox_inches='tight')
+        plt.show()
         return 'AUC: {:.4f}'.format(auc)

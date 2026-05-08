8b0c0ec73e36c0eb5d61ecf04d61e1b3f972ca3b
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2026-05-08 11:21:23 +0530

    minor updates

--- import_nci.py
+++ import_nci.py
@@ -7,8 +7,6 @@ password = "12345678"
 
 driver = GraphDatabase.driver(uri, auth=(user, password))
 
-supplier = Chem.SDMolSupplier("datasets/NCI_full/1total-connect.sdf")
-
 def clear_db(tx):
     tx.run("MATCH (n) DETACH DELETE n")
 
@@ -28,10 +26,11 @@ def create_bond(tx, mol_id, a1, a2, bond_type):
     tx.run("""
         MATCH (x:Atom {atom_id:$a1, molecule_id:$mid})
         MATCH (y:Atom {atom_id:$a2, molecule_id:$mid})
-        CREATE (x)-[:BOND {type:$btype}]-(y)
+        CREATE (x)-[:BOND {type:$btype}]->(y)
     """, a1=a1, a2=a2, mid=mol_id, btype=bond_type)
 
 def main():
+    supplier = Chem.SDMolSupplier("datasets/NCI_full/1total-connect.sdf")
     with driver.session() as session:
         session.execute_write(clear_db)
 
@@ -45,7 +44,8 @@ def main():
             label = mol.GetProp("value") if mol.HasProp("value") else "unknown"
 
             session.execute_write(create_molecule, mol_id, label)
-
+            count += 1
+            print(f"created molecule: {mol_id}, count - {count}")
             # atoms
             for atom in mol.GetAtoms():
                 session.execute_write(
@@ -64,8 +64,6 @@ def main():
                     bond.GetEndAtomIdx(),
                     int(bond.GetBondTypeAsDouble())
                 )
-
-            count += 1
 
             if count == 100:   # start with first 100 molecules
                 break

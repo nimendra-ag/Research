6e483486990ad740a9914db96ea0772e6102ac5e
levindu <levinduherath@gmail.com>
2026-05-12 20:40:45 +0530

    add random seed

--- graph_encoders/fsm.py
+++ graph_encoders/fsm.py
@@ -125,10 +125,3 @@ class FSM(GraphEncoder):
         # calc_coefficients will automatically only build columns for the valid features!
         return self.calc_coefficients(documents)
 
-    def generate_inferencing_embeddings(self, graphs):
-        """Extracts subgraphs from new data and maps them to the EXISTING vocabulary."""
-        if self.vocab is None:
-            raise ValueError("Vocabulary not built. Call generate_training_embeddings first.")
-
-        documents = self.extract_subgraphs(graphs)
-        return self.calc_coefficients(documents)

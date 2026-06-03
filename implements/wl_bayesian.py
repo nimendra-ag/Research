c2ea9a1ceab35c73c8f30b6dfbdd605524562159
Nimendra Gunawardana <nimendraamiththa1@gmail.com>
2026-06-03 13:30:12 +0530

    update with the latest wl_bayesia code

--- implements/wl_bayesian.py
+++ implements/wl_bayesian.py
@@ -5,7 +5,21 @@ from utils.evaluator import Evaluator
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MaxAbsScaler
 
-# data_loader = GraphDataLoader()
+data_loader = GraphDataLoader()
+
+graphs, y = data_loader.nci_full_graphs, data_loader.nci_full_labels
+
+G_train, G_test, y_train, y_test = train_test_split(
+    graphs, y,
+    test_size=0.2,
+    random_state=42
+)
+
+G_vocab_train, G_ML_train, y_vocab_train, y_ML_train = train_test_split(
+    G_train, y_train,
+    test_size=0.75,
+    random_state=42
+)
 
 #-------------------------Without overfit protection-------------------------#
 # # load graph data
@@ -62,19 +76,10 @@ class WL_BAYESIAN:
         self.implementation = "WL_BAYESIAN"
         self.data_loader = data_loader
 
-    def run(self):
-
-        # load graph data
-        graphs, y = self.data_loader.nci_full_graphs, self.data_loader.nci_full_labels
-        # First divide the data into train and test sets.
-        G_train, G_test, y_train, y_test = train_test_split(graphs, y, test_size=0.2, random_state=42)
-
-        # divide the train set further into vocab training and ML training sets
-        G_vocab_train, G_ML_train, y_vocab_train, y_ML_train = train_test_split(G_train, y_train, test_size=0.75,
-                                                                                random_state=42)
+    def run(self, G_vocab_train, y_vocab_train, G_ML_train, G_test, y_ML_train, y_test):
 
         wl = WL()
-        graph_embeddings = wl.generate_training_embeddings(G_vocab_train)
+        graph_embeddings = wl.generate_training_embeddings(G_vocab_train, y_vocab_train)
 
         bayesian_dl = BAYESIAN_DL().fit(training_graph_embeddings=graph_embeddings)
 
@@ -95,3 +100,6 @@ class WL_BAYESIAN:
 
         results_gradient_boosting = evaluator.predict_gradient_boosting()
         print(results_gradient_boosting)
+
+wl_bayesian = WL_BAYESIAN(data_loader)
+wl_bayesian.run(G_vocab_train, y_vocab_train, G_ML_train, G_test, y_ML_train, y_test)
